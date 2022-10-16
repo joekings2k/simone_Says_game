55d1f271b8268d713e70ebe4c212163d7e0180d5
Your Name <eromoselejonthan@gmail.com>
2022-10-16 06:31:55 +0100

    next

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern =[]
 
 
 
@@ -9,26 +10,43 @@ function nextSequence() {
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
   document.addEventListener("keydown",(event)=>{
-    fade()
-    playSound()
+    fade(randomChosenColor)
+    playSound(randomChosenColor)
     console.log(event)
   })
   
-  let fade = () => {
-    let link = $(`#${randomChosenColor}`);
-    link.fadeIn(100).fadeOut(100).fadeIn(100);
-  };
-
-  function playSound() {
-    const sound = new Audio(`./sounds/${randomChosenColor}.mp3`);
-    sound.play();
-  }
-  
-  
   
 
   
+  // 
+
+  let button= document.querySelectorAll(".btn")
+  for(let i=0 ;i<button.length;i++){
+    button[i].addEventListener("click",(e)=>{
+      let userChosenColor = e.target.id
+      userClickedPattern.push(userChosenColor)
+      fade(userChosenColor)
+      playSound(userChosenColor)
+    })
+    
+  }  
 }
 nextSequence();
 
 
+function playSound(name) {
+  const sound = new Audio(`./sounds/${name}.mp3`);
+  sound.play();
+}
+
+let fade = (color) => {
+  let link = $(`#${color}`);
+  link.fadeIn(100).fadeOut(100).fadeIn(100);
+};
+
+let animatePress = (selectedButton) => {
+  let buttons = $(".btn");
+  buttons.click((event)=>{
+    
+  })
+};
