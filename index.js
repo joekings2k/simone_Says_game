8353631129524d389ba5dc0ce608d2baed5e88fa
Your Name <eromoselejonthan@gmail.com>
2022-11-01 12:41:07 +0100

    next

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-let userClickedPattern =[]
+let userClickedPattern = [];
 
 
+let level = 0;
+let started  = false
+
+document.addEventListener("keydown",()=>{
+  if (!started){
+    nextSequence();
+
+    let h1 = document.querySelector("h1");
+    h1.innerHTML = `level ${level}`;
+    started = true
+  }
+  
+})
+
+
+let button = document.querySelectorAll(".btn");
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", (e) => {
+    let userChosenColor = e.target.id;
+    userClickedPattern.push(userChosenColor);
+    let lengthAnswer = userClickedPattern.length-1
+    console.log(lengthAnswer)
+    // fade(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(lengthAnswer)
+  });
+}
 
 function nextSequence() {
+
+
+  level++;
   let randomNumber = Math.random() * 4;
   randomNumber = Math.floor(randomNumber);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  document.addEventListener("keydown",(event)=>{
-    fade(randomChosenColor)
-    playSound(randomChosenColor)
-    console.log(event)
-  })
   
   
+  fade(randomChosenColor);
+  playSound(randomChosenColor);
 
   
-  // 
-
-  let button= document.querySelectorAll(".btn")
-  for(let i=0 ;i<button.length;i++){
-    button[i].addEventListener("click",(e)=>{
-      let userChosenColor = e.target.id
-      userClickedPattern.push(userChosenColor)
-      fade(userChosenColor)
-      playSound(userChosenColor)
-    })
-    
-  }  
+  
 }
-nextSequence();
 
 
 function playSound(name) {
@@ -44,9 +60,22 @@ let fade = (color) => {
   link.fadeIn(100).fadeOut(100).fadeIn(100);
 };
 
-let animatePress = (selectedButton) => {
-  let buttons = $(".btn");
-  buttons.click((event)=>{
-    
-  })
-};
+
+function animatePress(currentcolor){
+  $(`#${currentcolor}`).addClass("pressed")
+
+  setTimeout(()=>{
+    $(`#${currentcolor}`).removeClass("pressed");
+  },200)
+}
+
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
+    console.log("success")
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(()=>{nextSequence()},1000)
+    }
+  }else{
+    console.log("try again")
+  }
+}
